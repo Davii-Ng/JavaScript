3dbf2e16adbceb41996477db3931a8198c43b9e9
Davii-Ng <nminhd98@gmail.com>
2025-09-24 02:42:11 +0700

    first commit

---
+++ Lesson 3/main.js
@@ -0,0 +1,20 @@
+// 3a 
+// Create the text 'My name is' as a strin
+'My name is'
+
+// 3b
+// Create your name as a string
+'David'
+
+//3c
+// Concatenate the two strings above to create the string 'My name is David'
+'My name is: ' + 'David'
+
+//3d
+// At a restaurant, you order 1 coffee and  1 bagel. Use math to calculate cost and concatentation to create the string 'Your total is $5.50'
+5 + 3
+'Total cost: ' + '$' + 5+3
+
+//3e
+//Same thing but use template string and interpolation
+`Total cost: $${5 + 3}`
